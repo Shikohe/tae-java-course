e21b869c7ff8382120ba6ec0b7225edf31a92260
Mikheili Gogokhia <mikheilgogokhia@credo.ge>
2026-05-29 16:40:11 +0400

    [Mikheil Gogokhia]HW4-fixed mistakes

--- src/main/java/homework4/Factorial.java
+++ src/main/java/homework4/Factorial.java
@@ -1,4 +1,14 @@
 package homework4;
+//author misho gogokhia
 
 public class Factorial {
+    public static void main(String[] args) {
+        int birthMonth = 6;
+        long factorial = 1;
+        System.out.println("[Misho Gogokhia]Task5");
+        for (int i=1;i<=birthMonth;i++){
+            factorial *= i;
+        }
+        System.out.println(birthMonth+"!= " + factorial);
+    }
 }
--- src/main/java/homework4/SumOfDigits.java
+++ src/main/java/homework4/SumOfDigits.java
@@ -1,4 +1,17 @@
 package homework4;
-
+//author misho gogokhia
 public class SumOfDigits {
+    public static void main(String[] args) {
+        System.out.println("[Misho Gogokhia]Task6");
+        int year = 2004;
+        int sum = 0;
+
+        while (year > 0) {
+
+            sum += year % 10;
+            year /= 10;
+        }
+
+        System.out.println("Sum of digits: " + sum);
+    }
 }
