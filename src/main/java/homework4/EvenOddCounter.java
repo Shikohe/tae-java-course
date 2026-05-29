2b609b91e6bfa0c314bc0905614b01e78057ab4c
Mikheili Gogokhia <mikheilgogokhia@credo.ge>
2026-05-29 16:06:53 +0400

    [Mikheil Gogokhia]HW4-EvenOddCounter,add something which was missed

--- src/main/java/homework4/EvenOddCounter.java
+++ src/main/java/homework4/EvenOddCounter.java
@@ -1,4 +1,21 @@
 package homework4;
+//author misho gogokhia
+public class EvenOddCounter {
+    public static void main(String[] args) {
+            int birthYear = 2004;
+            int lastTwoDigits = birthYear % 100;
 
-public class EvenoddCounter {
+            int even = 0;
+            int odd = 0;
+        System.out.println("[Misho Gogokhia]Task4");
+            for(int i=1;i<=lastTwoDigits;i++){
+                if (i%2== 0){
+                    even++;
+                } else{
+                    odd++;
+                }
+            }
+        System.out.println("Even: "+even);
+        System.out.println("Odd: "+ odd);
+    }
 }
