11174465d684e03e263385ed6bf24bc9cfbf4991
Misho <mikheilgogokhia@credo.ge>
2026-06-10 15:23:50 +0400

    HW7 — Task 6: Vehicle abstract class

---
+++ src/main/java/homework7/Bike.java
@@ -0,0 +1,13 @@
+package homework7;
+
+public class Bike extends Vehicle {
+
+    public Bike(String brand) {
+        super(brand);
+    }
+
+    @Override
+    public void start() {
+        System.out.println(brand + " pedaling...");
+    }
+}
