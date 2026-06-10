93bba32e2d632f49b239e96d0a7a0f2a68cea440
Misho <mikheilgogokhia@credo.ge>
2026-06-10 16:05:25 +0400

    HW7 — Task 8: Employee hierarchy

---
+++ src/main/java/homework7/Employee.java
@@ -0,0 +1,16 @@
+package homework7;
+
+public abstract class Employee {
+    protected String name;
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+
+    public abstract double calculateSalary();
+
+    public void printInfo() {
+        System.out.println(name + " — Salary: " + calculateSalary());
+    }
+}
