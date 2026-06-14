e912d3a6a33c01bff109f7ca073807fe64c0e6bb
Misho <mikheilgogokhia@credo.ge>
2026-06-14 16:59:38 +0400

    [Mikheili G.] HW8 - Fixed ArrayReader with ArrayIndexOutOfBoundsException

--- src/main/java/homework8/ArrayReader.java
+++ src/main/java/homework8/ArrayReader.java
@@ -1,4 +1,22 @@
 package homework8;
 
+/** * @author Mikheili G.
+ */
 public class ArrayReader {
+
+    public static int readSafe(int[] arr, int index) {
+        try {
+            return arr[index];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("არასწორი ინდექი: " + index);
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {10, 20, 30};
+
+        System.out.println("სწორი ინდექსი (1): " + readSafe(nums, 1));
+        System.out.println("არასწორი ინდექსი (10): " + readSafe(nums, 10));
+    }
 }
