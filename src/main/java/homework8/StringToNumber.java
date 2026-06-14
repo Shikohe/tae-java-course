d38599454b1fe0a2b8929a850001af22fb992142
Misho <mikheilgogokhia@credo.ge>
2026-06-14 16:59:44 +0400

    [Mikheili G.] HW8 - Fixed StringToNumber with NumberFormatException

--- src/main/java/homework8/StringToNumber.java
+++ src/main/java/homework8/StringToNumber.java
@@ -1,4 +1,21 @@
 package homework8;
 
+/** * @author Mikheili G.
+ */
 public class StringToNumber {
+
+    public static int parseSafe(String s) {
+        try {
+            return Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            System.out.println("არ არის რიცხვი: " + s);
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("ტესტი 1 (\"42\"): " + parseSafe("42"));
+        System.out.println("ტესტი 2 (\"abc\"): " + parseSafe("abc"));
+        System.out.println("ტესტი 3 (\"\"): " + parseSafe(""));
+    }
 }
